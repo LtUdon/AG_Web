62827633d9cdc34211c5ed8e193b9b78ee4e2fc2
Gian Alfonso <alfonsogian225@gmail.com>
2026-03-14 23:19:32 -0500

    Project: Video render fix

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -37,43 +37,54 @@ return (
 					<h2>Esoterra</h2>
 					<p>
 						A third-person exploration/survival game where the player must explore special intrests within and information of the island.
-						<ul>
-							<li>A collaborative project initially involving 7 developers.</li>
-							<li>In the programming side, I was tasked with:</li>
-							<ul>
-								<li>Creating an interactive inventory system to promote ease-of-use equippment management to adapt to a high-paced environment.</li>
-								<li>Creating an immersive crafting system that efficiently interacts with the inventory system, updating the player's inventory contents based crafting requirements.</li>
-							</ul>
-							<li>In the creative side, I was involved in designing the island and pin-pointing special intrests for players to explore.</li>
-							<li><a href='https://www.youtube.com/watch?v=qncXzy0MQb0'>Watch trailer here</a></li>
-						</ul>
 					</p>
-					<a href='https://files.luckydiestudios.org/Esoterra/' className='anchor-reference'>
-						<img src={esoterra_Map} alt="Esoterra Map"/>
-						<img src={esoterra_Temple} alt="Esoterra Temple"/>
+					<ul>
+						<li>A collaborative project initially involving 7 developers.</li>
+						<li>In the programming side, I was tasked with:</li>
+						<ul>
+							<li>Creating an interactive inventory system to promote ease-of-use equippment management to adapt to a high-paced environment.</li>
+							<li>Creating an immersive crafting system that efficiently interacts with the inventory system, updating the player's inventory contents based crafting requirements.</li>
+						</ul>
+						<li>In the creative side, I was involved in:</li>
+						<ul>
+							<li>Finalizing the base design island.</li>
+							<li>Pin-pointing special intrests for players to explore.</li>
+						</ul>
+						<li><a href='https://files.luckydiestudios.org/Esoterra/'>Visit downolad page here</a></li>
+					</ul>
+					
+					<a 
+					href='https://files.luckydiestudios.org/Esoterra/' 
+					className='anchor-reference'>
+						<img 
+						src={esoterra_Map} 
+						alt="Esoterra Map"
+						title='UI of the map showcasing the island. CLICK for download page!'/>
+						<img 
+						src={esoterra_Temple} 
+						alt="Esoterra Temple"
+						title='One of the seven temples present on the island. CLICK for download page!'/>
 					</a>
 					<iframe
-					width={"100%"}
-					src="https://www.youtube.com/watch?v=qncXzy0MQb0"
-					title="https://www.youtube.com/watch?v=qncXzy0MQb0"
-					allowFullScreen
+					src="https://www.youtube.com/embed/qncXzy0MQb0"
+					title="https://www.youtube.com/embed/qncXzy0MQb0"
 					/>
 				</li>
 				<li className='project-token'>
 					<h2>Orion Project</h2>
 					<p>
 						A minimal Real-time strategy game where the player must achieve full control in a system to win. <br/>
+					</p>
 						<ul>
 							<li>A personal project that employs a strict architecture for practicing compatability between the C++ and Unreal Engine's Blueprints.</li>
 						<li>In a given class hierarchy, <b>C++</b> was used for classes that have children while Unreal Engine's <b>Blueprints</b> were used for classes that did not.</li>
 						</ul>
-
-					</p>
 				</li>
 				<li className='project-token'>
 					<h2>Volunteering Website</h2>
 					<p>
 						A volunteering-matching website that allows users to match volunteering events with their skills.
+					</p>
 						<ul>
 							<li>A team-based assignment website of 4 people to employ our skills in full-stack development and database querying logic.</li>
 							<li>The user can:</li>
@@ -83,16 +94,15 @@ return (
 								<li>With admin controls, they can create events themselves and apply relevant skills recommended.</li>
 							</ul>
 						</ul>
-					</p>
 				</li>
 				<li className='project-token'>
 					<h2>Project</h2>
 					<p>
 						Description
+					</p>
 						<ul>
 							<li>Item</li>
 						</ul>
-					</p>
 				</li>
 			</ul>
 		</div>
